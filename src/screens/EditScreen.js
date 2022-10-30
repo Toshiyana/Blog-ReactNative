7fc86c7911361d2295607e709c00228eebf1247e
Toshiyana <yanagimoto-toshiki@dmm.com>
2022-10-30 18:16:57 +0900

    Editing in Reducer

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -4,7 +4,8 @@ import { Context } from "../context/BlogContext";
 import BlogPostForm from "../components/BlogPostForm";
 
 const EditScreen = ({ navigation }) => {
-  const { state } = useContext(Context);
+  const id = navigation.getParam("id");
+  const { state, editBlogPost } = useContext(Context);
 
   const blogPost = state.find(
     (blogPost) => blogPost.id === navigation.getParam("id")
@@ -14,7 +15,7 @@ const EditScreen = ({ navigation }) => {
     <BlogPostForm
       initialValues={{ title: blogPost.title, content: blogPost.content }}
       onSubmit={(title, content) => {
-        console.log(title, content);
+        editBlogPost(id, title, content);
       }}
     />
   );
